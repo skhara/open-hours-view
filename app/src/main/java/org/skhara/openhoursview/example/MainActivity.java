1d8c71745f5699bce4138f63369069f0e2ed5250
sekha <sergey.oxygen@gmail.com>
2018-08-01 11:03:47 +0300

    Stub project

---
+++ app/src/main/java/org/skhara/openhoursview/example/MainActivity.java
@@ -0,0 +1,15 @@
+package org.skhara.openhoursview.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.skhara.openhoursview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
